a8275918729f512e750ecf0f24e2b62397d0611d
Louis Machin <ldhmachin@gmail.com>
2022-05-08 13:50:22 +0100

    Added getPlaying

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json());
 
 const PORT = 82;
+const METADATA_PATH = './metadata.json';
 
 app.get('/ping', (req, res) => {
     res.status(200).send('Pong!');
@@ -17,9 +18,18 @@ app.get('/setPlaying', (req, res) => {
         track: track,
         collection: collection
     }
-    fs.writeFileSync('./metadata.json', JSON.stringify(data));
+    fs.writeFileSync(METADATA_PATH, JSON.stringify(data));
     console.log(JSON.stringify(data));
     res.status(200).send('Submitted!');
+});
+
+app.get('/getPlaying', (req, res) => {
+    if (fs.existsSync(METADATA_PATH)) {
+        data = JSON.parse(fs.readFileSync(METADATA_PATH));
+        res.status(200).send(data);
+    } else {
+        res.status(204).send({});
+    }
 });
 
 app.listen(
