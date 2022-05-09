73e0f720f176a4c6784cbda4f701aa7fa252689f
Louis Machin <ldhmachin@gmail.com>
2022-05-09 20:52:23 +0100

    Added application_id

--- index.js
+++ index.js
@@ -5,28 +5,41 @@ const app = express();
 app.use(express.json());
 
 const PORT = 82;
-const METADATA_PATH = './metadata.json';
+const USERS_PATH = './users.json';
 
 app.get('/ping', (req, res) => {
     res.status(200).send('Pong!');
 });
 
 app.get('/setPlaying', (req, res) => {
-    const { artist, track, collection } = req.query;
-    data = {
+    const { application_id, artist, track, collection } = req.query;
+    player_data = {
         artist: artist,
         track: track,
         collection: collection
     }
-    fs.writeFileSync(METADATA_PATH, JSON.stringify(data));
-    console.log(JSON.stringify(data));
+    var user_data = JSON.parse(fs.readFileSync(USERS_PATH));
+    if (application_id in user_data) {
+        user_data[application_id].player = player_data;
+    } else {
+        user_data[application_id] = {
+            player: player_data
+        }
+    }
+    fs.writeFileSync(USERS_PATH, JSON.stringify(user_data));
+    console.log(application_id, JSON.stringify(player_data));
     res.status(200).send('Submitted!');
 });
 
 app.get('/getPlaying', (req, res) => {
-    if (fs.existsSync(METADATA_PATH)) {
-        data = JSON.parse(fs.readFileSync(METADATA_PATH));
-        res.status(200).send(data);
+    const { application_id } = req.query;
+    if (fs.existsSync(USERS_PATH)) {
+        var user_data = JSON.parse(fs.readFileSync(USERS_PATH));
+        if (application_id in user_data) {
+            res.status(200).send(user_data[application_id].player)
+        } else {
+            res.status(200).send({});
+        }
     } else {
         res.status(204).send({});
     }
