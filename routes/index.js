b6c7a29221efc2616f042760e6a1f4ba372a59e6
Louis Machin <ldhmachin@gmail.com>
2022-05-10 14:41:48 +0100

    Fixed bug

--- routes/index.js
+++ routes/index.js
@@ -36,7 +36,7 @@ app.locals.writeUserData = function () {
 }
 
 app.locals.isAdmin = function (application_id) {
-    if (validApplicationId(application_id)) {
+    if (app.locals.validApplicationId(application_id)) {
         return (app.locals.user_data[application_id].role & ROLE_ADMIN);
     } else {
         return false;
