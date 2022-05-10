2b2c3957a12cfcb0c7ce1c84db2122629fb3e9da
Louis Machin <ldhmachin@gmail.com>
2022-05-10 09:56:40 +0100

    Added getArtist

--- index.js
+++ index.js
@@ -60,6 +60,24 @@ app.get('/applicationId', (req, res) => {
     });
 });
 
+app.get('/getArtist', (req, res) => {
+    var { application_id, artist } = req.query;
+    if ((application_id != undefined) && (application_id in user_data)) {
+        artist = strToKey(artist);
+        var log_data = [`aid=${application_id}`, `artist=${artist}`];
+        if (artist in album_data) {
+            res.status(200).send(album_data[artist]);
+        } else {
+            // Missing artist
+            writeUncategorizedData(log_data);
+            res.status(204).send();
+        }
+    } else {
+        // Invalid application_id
+        res.status(401).send();
+    }
+});
+
 app.get('/getAlbum', (req, res) => {
     var { application_id, artist, album } = req.query;
     if ((application_id != undefined) && (application_id in user_data)) {
