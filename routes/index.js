ffe65ffdc908e48bf32579e132371f861db242d1
Louis Machin <ldhmachin@gmail.com>
2022-05-17 10:23:43 +0100

    Create post with user_id vote

--- routes/index.js
+++ routes/index.js
@@ -126,7 +126,7 @@ app.locals.createPost = function (application_id, title, body, tags) {
     app.locals.post_data[app.locals.generateId()] = {
         author: app.locals.application_data[application_id].user_id,
         posted_at: new Date().toISOString(),
-        votes: [application_id],
+        votes: [app.locals.application_data[application_id].user_id],
         tags: tags,
         contents: {
             title: title,
