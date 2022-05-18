4dbaef13a53fc3c9ff94a29de667dfbadd8c332f
Louis Machin <ldhmachin@gmail.com>
2022-05-18 13:01:48 +0100

    Testing GET for /setTopArtists

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,4 +1,15 @@
 module.exports = function (app) {
+    app.get('/setTopArtists', (req, res) => {
+        const { application_id } = req.query;
+        if (app.locals.validApplicationId(application_id)) {
+            console.log('GET /setTopArtists', application_id, req.body);
+            // app.locals.setUserStatistic(application_id, 'top_artists', req.body);
+            res.status(200).send();
+        } else {
+            res.status(401).send();
+        }
+    });
+
     app.post('/setTopArtists', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
