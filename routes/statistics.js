30286045de7bbe5595b12c83708f61ef7d583b50
Louis Machin <ldhmachin@gmail.com>
2022-05-18 13:05:52 +0100

    Test GET for statistics routes

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,17 +2,6 @@ module.exports = function (app) {
     app.get('/setTopArtists', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
-            console.log('GET /setTopArtists', application_id, req.body);
-            // app.locals.setUserStatistic(application_id, 'top_artists', req.body);
-            res.status(200).send();
-        } else {
-            res.status(401).send();
-        }
-    });
-
-    app.post('/setTopArtists', (req, res) => {
-        const { application_id } = req.query;
-        if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopArtists', application_id, req.body);
             app.locals.setUserStatistic(application_id, 'top_artists', req.body);
             res.status(200).send();
@@ -21,7 +10,7 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/setTopTracks', (req, res) => {
+    app.get('/setTopTracks', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopTracks', application_id, req.body);
@@ -32,7 +21,7 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/setTopGenres', (req, res) => {
+    app.get('/setTopGenres', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopGenres', application_id, req.body);
