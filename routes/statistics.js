71863d4ed6b93e757bd17f8d4485efd164a680d1
Louis Machin <ldhmachin@gmail.com>
2022-05-18 14:17:08 +0100

    Using temporary get requests for statistics

--- routes/statistics.js
+++ routes/statistics.js
@@ -4,7 +4,11 @@ module.exports = function (app) {
         if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopArtists', application_id, req.body);
             app.locals.setUserStatistic(application_id, 'top_artists', {
-                [aa]: ab, [ba]: bb, [ca]: cb, [da]: db, [ea]: eb,
+                0: { artist: aa, listens: ab },
+                1: { artist: ba, listens: bb },
+                2: { artist: ca, listens: cb },
+                3: { artist: da, listens: db },
+                4: { artist: ea, listens: eb }
             });
             res.status(200).send();
         } else {
@@ -12,22 +16,34 @@ module.exports = function (app) {
         }
     });
 
-    app.post('/setTopTracks', (req, res) => {
-        const { application_id } = req.query;
+    app.get('/setTopTracks', (req, res) => {
+        const { application_id, aa, ab, ac, ba, bb, bc, ca, cb, cc, da, db, dc, ea, eb, ec } = req.query;
         if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopTracks', application_id, req.body);
-            app.locals.setUserStatistic(application_id, 'top_tracks', req.body);
+            app.locals.setUserStatistic(application_id, 'top_tracks', {
+                0: { artist: aa, track: ab, listens: ac },
+                1: { artist: ba, track: bb, listens: bc },
+                2: { artist: ca, track: cb, listens: cc },
+                3: { artist: da, track: db, listens: dc },
+                4: { artist: ea, track: eb, listens: ec }
+            });
             res.status(200).send();
         } else {
             res.status(401).send();
         }
     });
 
-    app.post('/setTopGenres', (req, res) => {
-        const { application_id } = req.query;
+    app.get('/setTopGenres', (req, res) => {
+        const { application_id, aa, ab, ba, bb, ca, cb, da, db, ea, eb } = req.query;
         if (app.locals.validApplicationId(application_id)) {
             console.log('/setTopGenres', application_id, req.body);
-            app.locals.setUserStatistic(application_id, 'top_genres', req.body);
+            app.locals.setUserStatistic(application_id, 'top_genres', {
+                0: { genre: aa, listens: ab },
+                1: { genre: ba, listens: bb },
+                2: { genre: ca, listens: cb },
+                3: { genre: da, listens: db },
+                4: { genre: ea, listens: eb }
+            });
             res.status(200).send();
         } else {
             res.status(401).send();
