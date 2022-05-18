0dd510533cc036c8c1c2e9544ddcb1f72116f8ff
Louis Machin <ldhmachin@gmail.com>
2022-05-18 12:12:39 +0100

    Remove user_id from statistics post routes

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,41 +1,29 @@
 module.exports = function (app) {
     app.post('/setUserTopArtists', (req, res) => {
-        const { application_id, user_id } = req.query;
+        const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
-            if (app.locals.validUserId(user_id)) {
-                app.locals.setUserStatistic(application_id, 'top_artists', res.body);
-                res.status(200).send();
-            } else {
-                res.status(204).send();
-            }
+            app.locals.setUserStatistic(application_id, 'top_artists', res.body);
+            res.status(200).send();
         } else {
             res.status(401).send();
         }
     });
 
     app.post('/setUserTopTracks', (req, res) => {
-        const { application_id, user_id } = req.query;
+        const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
-            if (app.locals.validUserId(user_id)) {
-                app.locals.setUserStatistic(application_id, 'top_tracks', res.body);
-                res.status(200).send();
-            } else {
-                res.status(204).send();
-            }
+            app.locals.setUserStatistic(application_id, 'top_tracks', res.body);
+            res.status(200).send();
         } else {
             res.status(401).send();
         }
     });
 
     app.post('/setUserTopGenres', (req, res) => {
-        const { application_id, user_id } = req.query;
+        const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
-            if (app.locals.validUserId(user_id)) {
-                app.locals.setUserStatistic(application_id, 'top_genres', res.body);
-                res.status(200).send();
-            } else {
-                res.status(204).send();
-            }
+            app.locals.setUserStatistic(application_id, 'top_genres', res.body);
+            res.status(200).send();
         } else {
             res.status(401).send();
         }
