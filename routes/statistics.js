27737101cca1d086c5367aa0d33f10dbae5d1fe1
Louis Machin <ldhmachin@gmail.com>
2022-05-18 12:24:41 +0100

    Added logging

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,6 +2,7 @@ module.exports = function (app) {
     app.post('/setTopArtists', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
+            console.log('/setTopArtists', application_id, res.body);
             app.locals.setUserStatistic(application_id, 'top_artists', res.body);
             res.status(200).send();
         } else {
@@ -12,6 +13,7 @@ module.exports = function (app) {
     app.post('/setTopTracks', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
+            console.log('/setTopTracks', application_id, res.body);
             app.locals.setUserStatistic(application_id, 'top_tracks', res.body);
             res.status(200).send();
         } else {
@@ -22,6 +24,7 @@ module.exports = function (app) {
     app.post('/setTopGenres', (req, res) => {
         const { application_id } = req.query;
         if (app.locals.validApplicationId(application_id)) {
+            console.log('/setTopGenres', application_id, res.body);
             app.locals.setUserStatistic(application_id, 'top_genres', res.body);
             res.status(200).send();
         } else {
