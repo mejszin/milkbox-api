b12ed0488bbd6c93d02790573e41b58bd6b41f22
Louis Machin <ldhmachin@gmail.com>
2022-05-18 14:28:30 +0100

    Added post route for testing

--- routes/statistics.js
+++ routes/statistics.js
@@ -16,6 +16,17 @@ module.exports = function (app) {
         }
     });
 
+    app.post('/postSetTopTracks', (req, res) => {
+        const { application_id } = req.query;
+        if (app.locals.validApplicationId(application_id)) {
+            console.log('/postSetTopTracks', application_id, req.body);
+            app.locals.setUserStatistic(application_id, 'top_tracks', req.body);
+            res.status(200).send();
+        } else {
+            res.status(401).send();
+        }
+    });
+
     app.get('/setTopTracks', (req, res) => {
         const { application_id, aa, ab, ac, ba, bb, bc, ca, cb, cc, da, db, dc, ea, eb, ec } = req.query;
         if (app.locals.validApplicationId(application_id)) {
